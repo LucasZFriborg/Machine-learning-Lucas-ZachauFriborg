5a07283070aae8eb24699758ae295301e2f25914
LucasZFriborg <Lucas.ZachauFriborg@iths.se>
2026-04-30 20:30:25 +0200

    second draft of Functions.py

--- Labs/Assignment2/functions.py
+++ Labs/Assignment2/functions.py
@@ -9,74 +9,76 @@ from torchcam.methods import LayerCAM
 from torchcam.utils import overlay_mask
 
 
-weights = get_model_weights('resnet18').DEFAULT
-model = get_model('resnet18', weights=weights).eval()
+def load_model():
+    weights = get_model_weights('resnet18').DEFAULT
+    model = get_model('resnet18', weights=weights).eval()
+    preprocess = weights.transforms()
+     
+    return model, preprocess
 
-preprocess = weights.transforms()
+def get_classes(base_path):
+    classes = []
+    for name in os.listdir(base_path):
+        full_path = os.path.join(base_path, name)
+        if os.path.isdir(full_path):
+            classes.append(name)
+    return sorted(classes)
 
-base_path = 'Labs/Assignment2/Images'
-
-classes = os.listdir(base_path)
-
-def predict_class(output_tensor: torch.Tensor, class_index_path: str) -> dict:
-    with open(class_index_path, 'r') as f:
-        class_index = json.load(f)
-
-    probs = output_tensor.squeeze()
-    if probs.ndim != 1 or probs.shape[0] != 1000:
-        raise ValueError(
-            f'Expected a tensor of 1000 values, got shape {tuple(output_tensor.shape)}'
-        )
-    
-    top_idx = int(probs.argmax())
-    synset_id, class_name = class_index[str(top_idx)]
-
-    return {
-        'class index': top_idx,
-        'class_id': synset_id,
-        'class_name': class_name,
-        'confidence': float(probs[top_idx])
-    }
-
-for cls in classes:
-    class_path = os.path.join(base_path, cls)
-    print(class_path)
-
+def load_images(class_path):
     positive_path = os.path.join(class_path, 'positive.jpg')
     negative_path = os.path.join(class_path, 'negative.jpg')
-
+    
     img_positive = decode_image(positive_path)
     img_negative = decode_image(negative_path)
 
-    for label, img in [('positive', img_positive), ('negative', img_negative)]:
-        input_tensor = preprocess(img)
+    return {
+        'positive': img_positive,
+        'negative': img_negative
+    }
 
-        input_batch = input_tensor.unsqueeze(0)
-        output = model(input_batch)
+def run_inference(img, model, preprocess):
+    input_tensor = preprocess(img)
+    input_batch = input_tensor.unsqueeze(0)
 
-        prediction = output.squeeze(0).softmax(0)
+    output = model(input_batch)
+    prediction = output.squeeze(0).softmax(0)
 
-        prediction_result = predict_class(prediction.detach(), 'Labs/Assignment2/imagenet_class_index.json')
+    return output, prediction
 
-        print(f'Class: {cls} | Type: {label}')
-        print(prediction_result)
+def predict_class(prediction, class_index_path):
+    with open(class_index_path, 'r') as f:
+        class_index = json.load(f)
+    
+    top_idx = int(prediction.argmax())
+    synset_id, class_name = class_index[str(top_idx)]
 
-        with LayerCAM(model) as cam_extractor:
-            activation_map = cam_extractor(output.squeeze(0).argmax().item(), output)
+    return {
+        'class_index': top_idx,
+        'class_id': synset_id,
+        'class_name': class_name,
+        'confidence': float(prediction[top_idx])
+    }
 
-        plt.imshow(activation_map[0].squeeze(0).numpy())
-        plt.title(f'{cls} - {label}')
-        plt.axis('off')
-        plt.tight_layout()
-        plt.show()
+def generate_cam(model, output):
+    with LayerCAM(model) as cam_extractor:
+        activation_map = cam_extractor(output.squeeze(0).argmax().item(), output)
+    return activation_map
 
-        overlay_result = overlay_mask(
-            to_pil_image(img), 
-            to_pil_image(activation_map[0].squeeze(0), mode='F'),
-            alpha=0.5
-        )
-        plt.imshow(overlay_result)
-        plt.title(f'{cls} - {label} (overlay)')
-        plt.axis('off')
-        plt.tight_layout()
-        plt.show()
+def display_cam(activation_map, title):
+    plt.imshow(activation_map[0].squeeze(0).numpy())
+    plt.title(title)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+def display_overlay(img, activation_map, title):
+    result = overlay_mask(
+        to_pil_image(img),
+        to_pil_image(activation_map[0].squeeze(0), mode='F'), 
+        alpha=0.5
+    )
+    plt.imshow(result)
+    plt.title(title)
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
